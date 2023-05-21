76bad212b7bff58c5dfb3c34f2eac5c2aa773c9e
johnthairukamau <tkamau82@gmail.com>
2023-05-22 00:24:24 +0300

    Completed assignment

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,61 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+
+result = admin_login("ADMIN", "12345")
+print(result)
+
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 <= temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
 
-def fizzbuzz(num):
-    # your code here
-    pass
+temperature = 66
+result = hows_the_weather(temperature)
+print(result)
+
+
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+fizzbuzz()
+
+
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            print("Error: Division by zero is not allowed!")
+            return None
+    else:
+        print("Invalid operation!")
+        return None
+
+result = calculator('+', 5, 3)
+print(result)  # Output: 8
